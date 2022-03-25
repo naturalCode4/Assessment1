aee7944c3abdbb5878f7373805def8f66ac8423f
naturalCode4 <naturalCode4@gmail.com>
2022-03-25 11:00:18 -0600

    Adin_Assessment_1

---
+++ sldc.js
@@ -0,0 +1,26 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. The purpose and functionality of the project is summarized. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirements = "This is the second step, where the team of developers will assess what tools, technologies, and methodologies they will use to develop, write, build, and deploy the software. Analysis may involve discussing economic feasibility, time constraints, and the knowledge-base of the coders within the company."
+const design = 'This is the third step, where the team determines specifically they will build the total system, what will be the software and network infrastructure involved, and how all the pieces of code fit together. If this project takes place within a greater company, there should also be design in terms of integration with the greater system. Technical blueprints, flow-charts, and pseudo-code may be written. Further analysis of who-does-what and who-can-do-what may be necessary.'
+const implementation = 'This is the fourth step, the actual writing of code. The team will write in functional code, usually with the assistance of documentation like MDN and Stackoverflow, and often by grabbing pre-written code from other public projects as a foundation. Debugging and testing included. It is also important to write comments to explain the code, as its often not obvious what is going on in the code once one has stepped aside from the code for a while (a year, a month, even a few hours); it also may be essential for other developers to understand what is going on in your code and collaborate.'
+const testingAndIntegration = 'This is the fifth step, where the team sees that everything works as it is intended. It is deployed so that is available to those that are going to use it (i.e. web users or other developers). UI is considered and optimized.'
+const maintainence = 'This is the sixth step, and usually goes on indefinitely after the initial deployment of a satisfactory first software. This involves debugging edge cases, updating, improving, and accomodating new projects linked to the original software. This stage may also mean applying new information the company learns about client expectation and experience.'
