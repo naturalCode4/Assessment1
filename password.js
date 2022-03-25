4673e1d30ea2b972bea74d46e9ad9111b2f544ae
naturalCode4 <naturalCode4@gmail.com>
2022-03-25 12:17:11 -0600

    is_it_working

--- password.js
+++ password.js
@@ -1,27 +1,38 @@
 const readline = require('readline');
 
-const reader = readline.createInterface({
+const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-reader.question("Hello! What's your name?", function(input){
-    let userName = input
+/*
+rl.question("Hello, Please enter your password.", function(input){
+    let isTenChars
+    if (input.length >=10) {
+      isTenChars = true
+    }
 
-    reader.close()
-}
-
-reader.question(`Hello ${userName}! Please enter your password`, function(input){
-	
-    let tokens = input.split(' ');
-	let mathSymbol = tokens[0];
-	let num1 = Number(tokens[1]);
-	let num2 = Number(tokens[2]);
-	let answer 
-
-
-
-
-
-	reader.close()
+    rl.close()
 });
+*/
+
+rl.question('Hello beautiful!! Whats your name?:', (name) => {
+  rl.question(`Hello ${name}, What's your password:`, (password) => {
+
+      if (password.length >=10) {
+      console.log(`Your password is at least 10 characters. Good job, ${name}`)
+    } else console.log(`Your password is less than 10 characters. Sorry ${name}`)
+
+      rl.close();
+  });
+});
+/*
+const question1 = () => {
+  return new Promise((resolve, reject) => {
+    rl.question('q1 What do you think of Node.js? ', (answer) => {
+      console.log(`Thank you for your valuable feedback: ${answer}`)
+      resolve()
+    })
+  })
+}
+*/
